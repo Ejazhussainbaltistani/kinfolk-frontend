23931bf284e4151eb792dc28ca9bf96daf46db3a
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-30 03:27:56 +0500

    Created Details card

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import { DetailCards } from "./components";
 function App() {
   return (
     <div className="App">
-      <DetailCards />
+      <DetailCards
+        editCard={function (a: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      >
+        <div>
+          <h1>Ejaz hussain bala</h1>
+          <p>Ejaz hussain baltistani</p>
+        </div>
+      </DetailCards>
     </div>
   );
 }
--- src/components/DetailCards/DetailCards.tsx
+++ src/components/DetailCards/DetailCards.tsx
@@ -1,12 +1,23 @@
-export function DetailCards() {
+import { ReactElement } from "react";
+
+import { ReactComponent as IconEditCard } from "../../assets/icons/icon-edit-card.svg";
+import "./DetailCards.css";
+
+interface DetailCardsProps {
+  children: ReactElement;
+  editCard: (a: string) => void;
+}
+export function DetailCards({ children, editCard }: DetailCardsProps) {
   return (
     <div>
-      <div className="card" style={{ width: "18rems" }}>
-        <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+      <div className="card px-4 py-3" style={{ width: "25rem" }}>
+        <div className="flex-box">
+          <div className="card-body">
+            <p className="card-text">{children}</p>
+          </div>
+          <div className="dot">
+            <IconEditCard />
+          </div>
         </div>
       </div>
     </div>
